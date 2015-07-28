5e89fd69de0cc97d00c4219b0d3c732c3ac34651
Hack Reactor Students <accounts@hackreactor.com>
2015-07-28 11:33:06 -0700

    Add timestamps to user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,8 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+  hasTimestamps: true
 });
 
 module.exports = User;
